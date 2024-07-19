ba0cac0517804ef13d5cc2017de5b69e86095ea6
Dima Vorobyev <dima02081991@mail.ru>
2024-07-19 22:57:57 +0300

    modification

--- db_client.py
+++ db_client.py
@@ -59,57 +59,64 @@ class PYTHONDBClient:
             except psycopg2.errors.ForeignKeyViolation:
                 print('Клиент не существует.')
 
-    def change_client(self):
-
-        couse_id = input('Введите id клиента ')
+    def change_client(self, client, first_name='', last_name='', email='', phone=''):
+ 
         with self.conn.cursor() as cur:
-            cur.execute("""
-                SELECT first_name, last_name, email, id FROM clients 
-                WHERE id=%s ;
-                """, (int(couse_id),))
-            info_client = list(cur.fetchone())
-
-            cur.execute("""
-                SELECT phone, client, id FROM phones
-                WHERE client=%s ;
-                """, (int(couse_id),))
-            info_phones = (cur.fetchall())
-            print(f'Информация о клиетет {info_client[0]} {info_client[1]} {info_client[2]}')
-            print('Телефоны клиента и id:')
-            for phone in info_phones:
-                print (f'{phone[0]} {phone[2]}')
-            print('Напишите изменния в том же порядке, если нет изменения нажимите пробел и enter')
-            changes = []
-            changes.append(input('Имя '))
-            changes.append(input('Фамилия '))
-            changes.append(input('Email '))
-
-            recorded_changes =[]
-            
-            for c ,i in zip(changes, info_client): 
-                if c == ' ':
-                    recorded_changes.append(i)
-                else:
-                    recorded_changes.append(c)
-            
-            print('Выберите id телефона который хотите изменить, если нет изменения нажимите пробел и enter')
-            couse_id_phone = int(input())
-            if couse_id_phone == ' ':
-                print('Телефон без изменений ')
+            if first_name != '':
+                cur.execute("""
+                            UPDATE clients SET first_name=%s WHERE id=%s;
+                            """, (first_name,client ))
+                self.conn.commit()
+                print('Имя изменено.')
             else:
-                couse_phone = input('Введите телефон :')
+                print('Имя без изменений.')
 
-            cur.execute("""
-                UPDATE clients SET first_name=%s, last_name=%s, email=%s WHERE id=%s;
-                """, (recorded_changes[0], recorded_changes[1], recorded_changes[2], couse_id))
-            
-            cur.execute("""
-                UPDATE phones SET phone=%s WHERE id=%s;
-                """, (couse_phone, couse_id_phone))
-        
-        self.conn.commit()
-        print('Данные изменены.')
-    
+            if last_name != '':
+                cur.execute("""
+                            UPDATE clients SET last_name=%s WHERE id=%s;
+                            """, (last_name,client ))
+                self.conn.commit()
+                print('Фамилия изменена.')
+            else:
+                print('Фамилия без изменений.')
+
+            if email != '':
+                cur.execute("""
+                            UPDATE clients SET email=%s WHERE id=%s;
+                            """, (email,client ))
+                self.conn.commit()
+                print('Email изменен.')
+            else:
+                print('Email без изменений.')
+
+            if phone != '':
+                cur.execute("""
+                        SELECT phone, client, id FROM phones
+                        WHERE client=%s ;
+                         """, (client,))
+                info_phones = (cur.fetchall())
+
+                if len(info_phones) != 1:
+                    print('Выберите id телефона который хотите изменить :')
+                    for p in info_phones:
+                        print (f'{p[0]} {p[2]}')
+
+                    couse_id_phone = int(input())
+                    print(couse_id_phone)
+
+                    cur.execute("""
+                            UPDATE phones SET phone=%s WHERE id=%s;
+                            """, (phone, couse_id_phone))
+                    self.conn.commit()
+                    print('Телефон изменен.')
+                else:
+                    cur.execute("""
+                            UPDATE phones SET phone=%s WHERE id=%s;
+                            """, (phone,client))
+                    self.conn.commit()
+                    print('Телефон изменен.')
+            else:
+                print('Телефон без изменений.')
 
     def delete_phone(self, phone, client):
 
@@ -121,8 +128,13 @@ class PYTHONDBClient:
 
         print('Телефон клиента удален !')
    
-
     def delete_client(self, id):
+
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                DELETE FROM phones WHERE client=%s ;
+                """, ( id,))
+        self.conn.commit()
 
         with self.conn.cursor() as cur:
             cur.execute("""
@@ -131,88 +143,29 @@ class PYTHONDBClient:
         self.conn.commit()
 
         print('Клиент удален !')
-    
-    def show_client_name(self, first_name):
 
-        with self.conn.cursor() as cur:
+    def show_client(self, first_name='', last_name='', email='', phone=''):
+       with self.conn.cursor() as cur:
             cur.execute("""
-                SELECT first_name, last_name, email, id FROM clients 
-                WHERE first_name=%s ;
-                """, (first_name,))
-            info_client = list(cur.fetchall())
-            for client in info_client:
-                print(f'Информация о клиенте {client[0]} {client[1]} {client[2]} ')
+                SELECT first_name, last_name, email, client, phone FROM clients
+                JOIN phones ON clients.id = phones.client
+                WHERE first_name LIKE %s or last_name LIKE %s or email LIKE %s or phone LIKE %s;
+                """, (first_name,last_name,email,phone,))
+            info = cur.fetchall()
 
-                cur.execute("""
-                    SELECT phone, client, id FROM phones
-                    WHERE client=%s ;
-                    """, (int(client[3]),))
-                info_phones = (cur.fetchall())
-                print('Телефоны клиента :')
-                for p in info_phones:
-                    print (f'{p[0]}')
-
-    def show_client_last_name(self, last_name):
-
-        with self.conn.cursor() as cur:
-            cur.execute("""
-                SELECT first_name, last_name, email, id FROM clients 
-                WHERE last_name=%s ;
-                """, (last_name,))
-            info_client = list(cur.fetchall())
-            for client in info_client:
-                print(f'Информация о клиенте {client[0]} {client[1]} {client[2]} ')
-
-                cur.execute("""
-                    SELECT phone, client, id FROM phones
-                    WHERE client=%s ;
-                    """, (int(client[3]),))
-                info_phones = (cur.fetchall())
-                print('Телефоны клиента :')
-                for p in info_phones:
-                    print (f'{p[0]}')
-
-    def show_client_email(self, email):
-        with self.conn.cursor() as cur:
-
-            cur.execute("""
-                SELECT first_name, last_name, email, id FROM clients 
-                WHERE email=%s ;
-                """, (email,))
-            info_client = list(cur.fetchall())
-            for client in info_client:
-                print(f'Информация о клиенте {client[0]} {client[1]} {client[2]} ')
-
-                cur.execute("""
-                    SELECT phone, client, id FROM phones
-                    WHERE client=%s ;
-                    """, (int(client[3]),))
-                info_phones = (cur.fetchall())
-                print('Телефоны клиента :')
-                for p in info_phones:
-                    print (f'{p[0]}')
-
-    def show_client_phone(self, phone):
-
-        with self.conn.cursor() as cur:
-            cur.execute("""
-                    SELECT phone, client, id FROM phones
-                    WHERE phone=%s ;
-                    """, (phone,))
-            info_phone = (cur.fetchone())
-            cur.execute("""
-                    SELECT phone, client FROM phones
-                    WHERE client=%s ;
-                    """, (info_phone[1],))
-            info_phones = (cur.fetchall())
-            cur.execute("""
-                    SELECT first_name, last_name, email, client FROM clients 
-                    JOIN phones ON clients.id = phones.client 
-                    WHERE  client=%s;
-                    """, (info_phone[1],))
-            info_client = (cur.fetchone())
-
-            print(f'Информация о клиенте {info_client[0]} {info_client[1]} {info_client[2]} ')
-            print('Телефоны клиента :')
-            for p in info_phones:
-                print (f'{p[0]}')
+            if last_name != '':
+               for i in info :
+                    for item in i:
+                        if item == last_name:
+                            print(i)
+                        elif item == email:
+                            print(i)
+            elif  email != '':
+                for i in info :
+                    for item in i:
+                        if item == last_name:
+                            print(i)
+                        elif item == email:
+                            print(i)
+            else:
+                print(info)
